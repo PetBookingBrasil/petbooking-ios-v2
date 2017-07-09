9c34a061c2411dfdc6d1b2862e99b036ac995ba4
Ryniere <ryniere16@gmail.com>
2017-07-09 19:33:16 -0300

    color changing

--- petbooking-ios-v2/Modules/Presentation/PresentationInteractor.swift
+++ petbooking-ios-v2/Modules/Presentation/PresentationInteractor.swift
@@ -42,13 +42,13 @@ class PresentationInteractor: PresentationInteractorProtocol {
 		}
 		switch index {
 		case .first:
-			presenter?.setupView(imageNamed: "apt_01", backgroundColorHexString: "FFC500", title: NSLocalizedString("ap1_title", comment: ""), titleFont: nil, description: NSLocalizedString("ap1_description", comment: ""), descriptionFont: nil)
+			presenter?.setupView(imageNamed: "apt_01", backgroundColorHexString: "009CDE", title: NSLocalizedString("ap1_title", comment: ""), titleFont: nil, description: NSLocalizedString("ap1_description", comment: ""), descriptionFont: nil)
 			break
 		case .second:
-			presenter?.setupView(imageNamed: "apt_02", backgroundColorHexString: "FF7F0B", title:  NSLocalizedString("ap2_title", comment: ""), titleFont: nil, description:  NSLocalizedString("ap2_description", comment: ""), descriptionFont: nil)
+			presenter?.setupView(imageNamed: "apt_02", backgroundColorHexString: "F68D2E", title:  NSLocalizedString("ap2_title", comment: ""), titleFont: nil, description:  NSLocalizedString("ap2_description", comment: ""), descriptionFont: nil)
 			break
 		case .third:
-			presenter?.setupView(imageNamed: "apt_03", backgroundColorHexString: "FF4B39", title:  NSLocalizedString("ap3_title", comment: ""), titleFont: UIFont.systemFont(ofSize: 18), description:  NSLocalizedString("ap3_description", comment: ""), descriptionFont: UIFont.boldSystemFont(ofSize: 20))
+			presenter?.setupView(imageNamed: "apt_03", backgroundColorHexString: "9F5CC0", title:  NSLocalizedString("ap3_title", comment: ""), titleFont: UIFont.systemFont(ofSize: 18), description:  NSLocalizedString("ap3_description", comment: ""), descriptionFont: UIFont.boldSystemFont(ofSize: 20))
 			break
 		}
 		
